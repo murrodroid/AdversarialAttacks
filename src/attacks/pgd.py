6f3422c3266a3bef17b9676af6008a6a786b08c8
Rune Harlyk <rune@harlyk.dk>
2025-03-19 16:30:47 +0100

    ✨ Fixes interface of pgd

--- src/attacks/pgd.py
+++ src/attacks/pgd.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn as nn
 
-def pgd_attack(model, image, target_class, device, epsilon=0.3, alpha=0.01, max_iter=100):
+def pgd_attack(model, image, target_class, epsilon=0.3, alpha=0.01, max_iter=100):
   original = image.clone()
   adv = image.clone().detach() + torch.empty_like(image).uniform_(-epsilon/2, epsilon/2)
   adv = torch.clamp(adv, image.min(), image.max())
-  target = torch.tensor([target_class], device=device)
+  target = torch.tensor([target_class], device=image.device)
   criterion = nn.CrossEntropyLoss()
   for i in range(max_iter):
     adv.requires_grad = True
