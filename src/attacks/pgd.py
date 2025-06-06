fc1ac987c0011b3c17299af30cd5804cdff2d462
CelinaLaune <celina@laune.dk>
2025-06-06 10:01:06 +0200

    add batching code to PGD

--- src/attacks/pgd.py
+++ src/attacks/pgd.py
@@ -1,41 +1,45 @@
 import torch
 import torch.nn as nn
+from torch import Tensor
 
-def pgd_attack(model, image, target_class, epsilon=0.3, alpha=0.01, max_iter=100, break_early: bool = False):
-  original = image.clone()
-  adv = image.clone().detach() + torch.empty_like(image).uniform_(-epsilon/2, epsilon/2)
-  adv = torch.clamp(adv, image.min(), image.max())
-  target = torch.tensor([target_class], device=image.device)
-  criterion = nn.CrossEntropyLoss()
-  success = False
-
-  first_success_iter = None
-  first_success_output = None
-  final_output = None
-  
-  for i in range(max_iter):
-    adv.requires_grad = True
-    output = model(adv)
-    probs = torch.softmax(output, dim=1).detach()
-    pred_class = probs.argmax(dim=1).item()
+def pgd_attack(model: object, source_image: Tensor, target_class: list, epsilon=0.3, alpha=0.01, max_iter=100, break_early: bool = False):
+    perturbed_image = source_image.clone().detach() + torch.empty_like(source_image).uniform_(-epsilon/2, epsilon/2)
+    perturbed_image = torch.clamp(perturbed_image, source_image.min(), source_image.max())
+    target = torch.tensor(target_class, device=source_image.device)
+    criterion = nn.CrossEntropyLoss()
     
-    if pred_class == target_class:
-      if not success:
-          first_success_iter = i
-          first_success_output = probs.detach().clone().cpu().numpy().tolist()[0]
-      success = True
-      final_output = probs.detach().clone().cpu().numpy().tolist()[0]
-      if break_early: break
+    B = source_image.shape[0]
+    success = [False] * B
+    first_success_iter = [None] * B
+    first_success_output = [None] * B
+    final_output = [None] * B
     
-    loss = criterion(output, target)
-    model.zero_grad()
-    loss.backward()
-    
-    adv = adv.detach() - alpha * torch.sign(adv.grad.data)
-    eta = torch.clamp(adv - original, -epsilon, epsilon)
-    adv = torch.clamp(original + eta, image.min(), image.max())
+    for i in range(max_iter):
+        perturbed_image.requires_grad_(True)
+        output = model(perturbed_image)
+        probs = torch.softmax(output, dim=1).detach()
+        pred_classes = probs.argmax(dim=1)
+        
+        for j in range(B):
+              if pred_classes[j].item() == target[j].item():
+                    if not success[j]:
+                        first_success_iter[j] = i
+                        first_success_output[j] = probs[j].detach().clone().cpu().numpy().tolist()
+                    success[j] = True
+            
+        if break_early and all(success):
+              final_output = [probs[j].detach().clone().cpu().numpy().tolist() for j in range(B)]
+              break
+        
+        loss = criterion(output, target)
+        model.zero_grad()
+        loss.backward()
+        
+        perturbed_image = perturbed_image.detach() - alpha * torch.sign(perturbed_image.grad)
+        eta = torch.clamp(perturbed_image - source_image, -epsilon, epsilon)
+        perturbed_image = torch.clamp(source_image + eta, source_image.min(), source_image.max())
 
-    if i == max_iter - 1:
-        final_output = probs.detach().clone().cpu().numpy().tolist()[0]
+        if i == max_iter - 1:
+            final_output = [probs[j].detach().clone().cpu().numpy().tolist() for j in range(B)]
 
-  return adv, success, first_success_iter, first_success_output, final_output
+    return perturbed_image, success, first_success_iter, first_success_output, final_output
