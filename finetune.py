b921ec279cacc83348451f075f03a9757b3d54b6
Rune Harlyk <rune@harlyk.dk>
2025-06-13 10:29:36 +0200

    select model correct

--- finetune.py
+++ finetune.py
@@ -1,14 +1,14 @@
 from src.finetuning.base import finetune
 from src.finetuning.configs.base_finetune import train_cfg,wandb_cfg
 from src.datasets.imagenet import create_imagenet20_loaders
-from src.models.get_model import get_model
+from src.models.get_model import get_finetuned_model
 
 
 model_name = train_cfg["model_name"]
-model = get_model(model_name)
+
 def main() -> None:
     """Run finetuning for the configured model."""
-    model = get_model(model_name)
+    model = get_finetuned_model(model_name, cfg={"output_dim": 20})
     train_loader, val_loader = create_imagenet20_loaders(
         batch_size=train_cfg["batch_size"],
         workers=train_cfg["workers"],
