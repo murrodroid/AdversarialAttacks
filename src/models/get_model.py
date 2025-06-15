3d5c2f7874eddc6eb59adec516005885cbe00092
CelinaLaune <celina@laune.dk>
2025-06-15 16:54:14 +0200

    add get_robust_model

--- src/models/get_model.py
+++ src/models/get_model.py
@@ -7,6 +7,7 @@ import lzma
 
 from src.finetuning.base import _replace_head
 from src.utils.torch_util import getDevice
+from src.finetuning.custom_swin_backbone import build_custom_swin_tiny_with_temp
 
 
 def get_model(name):
@@ -18,7 +19,7 @@ def get_model(name):
     f, w = builders[name]
     return f(weights=w).eval().cuda() if torch.cuda.is_available() else f(weights=w).eval().cpu()
 
-def get_finetuned_model(name, device=getDevice(), cfg={"output_dim": 100}):
+def get_finetuned_model(name, device=getDevice(), cfg={"output_dim": 20}):
     """Get a finetuned model with the specified number of output classes.
 
     For models that don't have matching checkpoint files (like ImageNet20),
@@ -70,5 +71,24 @@ def get_finetuned_model(name, device=getDevice(), cfg={"output_dim": 100}):
                 model_state[key] = value
 
         model.load_state_dict(model_state)
+
+    return model.eval().to(device)
+
+def get_robust_model(name, device=getDevice(), cfg={"output_dim": 20}):
+    if name == "swin":
+        model = build_custom_swin_tiny_with_temp(
+            temperature=1.5,
+            num_classes=cfg["output_dim"]
+        )
+    else:
+        builders = dict(
+            mobilenet = mobilenet_v3_large,
+            resnet    = resnet50,
+        )
+        model = _replace_head(builders[name](weights=None), name, cfg)
+
+    ckpt = Path('src/models/weights') / f'{name}20.pt.xz'
+    with lzma.open(ckpt, "rb") as f:
+        model.load_state_dict(torch.load(f, map_location=device), strict=True)
 
     return model.eval().to(device)
